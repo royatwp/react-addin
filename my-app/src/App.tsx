3a86a9c00d87a87e4a85073ff6918992741dd816
Roy Stoliday <roy.stoliday@worldprogramming.com>
2022-01-27 16:35:54 +0000

    Some fudging around
    Change-Id: If0008f1f12e1b5009c1c8c089fa23e62a5e6bc0c

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import { Stack, Text, Link, FontWeights, IStackTokens, IStackStyles, ITextStyles } from '@fluentui/react';
-import logo from './logo.svg';
+import React, { SyntheticEvent } from 'react';
+
+import { Stack, Text, Link, FontWeights, IStackTokens, IStackStyles, ITextStyles, Label } from '@fluentui/react';
+import { DefaultButton, PrimaryButton } from '@fluentui/react/lib/Button';
+
+
 import './App.css';
 
+export interface IButtonExampleProps {
+  // These are set based on the toggles shown above the examples (not needed in real code)
+  disabled?: boolean;
+  checked?: boolean;
+}
+
 const boldStyle: Partial<ITextStyles> = { root: { fontWeight: FontWeights.semibold } };
-const stackTokens: IStackTokens = { childrenGap: 15 };
+const stackTokens: IStackTokens = { 
+  childrenGap: 15,
+ };
+
 const stackStyles: Partial<IStackStyles> = {
   root: {
     width: '960px',
@@ -14,25 +26,42 @@ const stackStyles: Partial<IStackStyles> = {
   },
 };
 
-export const App: React.FunctionComponent = () => {
+const myStackStyles: Partial<IStackStyles> = {
+  root: {
+    margin: '20px',
+    padding: '30px',
+    textAlign: 'center',
+    color: '#605e5c',
+    background: 'lightblue'
+  },
+};
+
+
+export const App: React.FunctionComponent<IButtonExampleProps> = props  => {
+
+  const { disabled, checked } = props;
+
+  const clickHandler = (e: any) => {
+    console.log(e)
+    alert('Clicked a thing')
+  }
+
   return (
     <Stack horizontalAlign="center" verticalAlign="center" verticalFill styles={stackStyles} tokens={stackTokens}>
-      <img className="App-logo" src={logo} alt="logo" />
+      <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam facilis et explicabo facere pariatur, quam dolorum accusantium quos commodi sequi amet nostrum culpa quibusdam saepe reiciendis rerum, fuga, sit ad molestiae excepturi sed sunt omnis. Debitis nihil modi fugit incidunt.</Text>
+      <Stack tokens={stackTokens} styles={myStackStyles} horizontal>
+        <Label>I am a label</Label>
+        <Label>I am another label</Label>
+        <Label>Label city</Label>
+      </Stack>
+      <DefaultButton text="Standard" onClick={clickHandler} allowDisabledFocus disabled={disabled} checked={checked} />
+      <PrimaryButton text="Primary" onClick={clickHandler} allowDisabledFocus disabled={disabled} checked={checked} />
+
       <Text variant="xxLarge" styles={boldStyle}>
         Welcome to your Fluent UI app
       </Text>
-      <Text variant="large">For a guide on how to customize this project, check out the Fluent UI documentation.</Text>
-      <Text variant="large" styles={boldStyle}>
-        Essential links
-      </Text>
-      <Stack horizontal tokens={stackTokens} horizontalAlign="center">
-        <Link href="https://developer.microsoft.com/en-us/fluentui#/get-started/web">Docs</Link>
-        <Link href="https://stackoverflow.com/questions/tagged/office-ui-fabric">Stack Overflow</Link>
-        <Link href="https://github.com/microsoft/fluentui/">Github</Link>
-        <Link href="https://twitter.com/fluentui">Twitter</Link>
-      </Stack>
-      <Text variant="large" styles={boldStyle}>
-        Design system
+      <Text variant="xxLarge" styles={boldStyle}>
+        Welcome to your Fluent UI app 2
       </Text>
       <Stack horizontal tokens={stackTokens} horizontalAlign="center">
         <Link href="https://developer.microsoft.com/en-us/fluentui#/styles/web/icons">Icons</Link>
@@ -40,5 +69,7 @@ export const App: React.FunctionComponent = () => {
         <Link href="https://aka.ms/themedesigner">Theme designer</Link>
       </Stack>
     </Stack>
+
+
   );
 };
